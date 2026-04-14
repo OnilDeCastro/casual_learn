c17e3e26636e96fbb1f75438ffed159cf8418389
onil.dc <onilmatavele@gmail.com>
2026-04-14 23:51:37 +0200

    primeiro commit

---
+++ app.py
@@ -0,0 +1,17 @@
+"""minha_primeira_variavel = "Agora, isto é uma string"
+print(type(minha_primeira_variavel))
+
+minha_primeira_variavel_1 = 100
+print(type(minha_primeira_variavel_1))"""
+
+#funcoes 
+
+def adicionar_dois_numeros(nr1, nr2):
+    soma = nr1 + nr2
+    print(adicionar_dois_numeros(3,4))
+
+"""
+def square_number(number):
+    square = number * number
+    return
+print(square_number(2))"""
---
+++ aula2.py
@@ -0,0 +1,11 @@
+my_first_variable = 1
+my_first_variable = 2
+print(type(my_first_variable))
+print(my_first_variable)
+
+my_second_variable = "Now im a string."
+print(type(my_second_variable))
+print(my_second_variable)
+
+my_third_variable = 'you can call me "str".'
+print(my_third_variable)
