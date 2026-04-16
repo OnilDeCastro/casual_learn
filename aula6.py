b85c1d51f98cd4c62ed2b3c261858df4562b8e56
onil.dc <onilmatavele@gmail.com>
2026-04-16 23:41:26 +0200

    Hoje foi sobre manipulaçao de strings

---
+++ aula6.py
@@ -0,0 +1,4 @@
+""" A auka de hoje sera sobre Strings/str os topicos serao tipos de texto sequencial metodos de string, common sequence operations, string e caracter dados em Python, formataçao de string, melhores praticas e string common string operations and formatting.
+"""
+
+
